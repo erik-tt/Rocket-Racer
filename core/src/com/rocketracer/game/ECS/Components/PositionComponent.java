329047d9703f71ee6cd030949f2771d1e23807b3
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-03-26 20:49:35 +0200

    #13 feat: create PositionComponent

---
+++ core/src/com/rocketracer/game/ECS/Components/PositionComponent.java
@@ -0,0 +1,15 @@
+package com.rocketracer.game.ECS.Components;
+import com.badlogic.ashley.core.Component;
+
+public class PositionComponent implements Component {
+    // --- Attributes ---
+    public float x;
+    public float y;
+
+    // --- Constructors ---
+    public PositionComponent(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
