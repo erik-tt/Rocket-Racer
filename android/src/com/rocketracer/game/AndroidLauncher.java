0af31680fb461c1ec7c39b88a1d7f74617f44b36
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-03-26 20:08:22 +0200

    #2 fix: fixed firebase errors

--- android/src/com/rocketracer/game/AndroidLauncher.java
+++ android/src/com/rocketracer/game/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.firebase.FirebaseApp;
 import com.rocketracer.game.Firebase.FirebaseHandler;
 import com.rocketracer.game.views.RocketRacer;
 
@@ -12,9 +13,9 @@ public class AndroidLauncher extends AndroidApplication {
 	private FirebaseHandler FBIHandler = new FirebaseHandler();
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
+		FirebaseApp.initializeApp(this);
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new RocketRacer(FBIHandler), config);
-
 	}
 }
