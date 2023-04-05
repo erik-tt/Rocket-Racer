1570a2cc4c65f785648dd1d442d30b80676acf3f
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-04-05 10:39:28 +0200

    [#17] feat: create render system

--- core/src/com/rocketracer/game/views/GameView.java
+++ core/src/com/rocketracer/game/views/GameView.java
@@ -1,22 +1,92 @@
 package com.rocketracer.game.views;
 
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.rocketracer.game.ECS.Entities.RocketEntity;
+import com.rocketracer.game.ECS.Systems.RenderSystem;
+import com.rocketracer.game.controllers.GameController;
 
 public class GameView implements Screen {
 
+    // --- Attributes ---
+    // Misc
+    private SpriteBatch batch;
+    protected Stage stage;
+    private Viewport viewport;
+    private Camera camera;
+    private TextureAtlas atlas;
+    protected Skin skin;
+
+    private GameController gameController;
+
+    // Navigation
+    /** For back button */
+    //private Screen prevScreen;
+
+    // Gameplay: will be moved to GameController
+    Engine engine;
+    RenderSystem renderSystem;
+    RocketEntity player = new RocketEntity();
+
+    // --- Constructor ---
+    /**
+     * Constructor.
+     * @param prevScreen The screen to return to when the back button is pressed.
+     */
+    public GameView(/*Screen prevScreen*/) {
+        atlas = new TextureAtlas("CustomSkin.atlas");
+        skin = new Skin(Gdx.files.internal("CustomSkin.json"), atlas);
+        batch = new SpriteBatch();
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(1080/5, 2340/5, camera);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+        stage = new Stage(viewport, batch);
+        gameController = new GameController();
+        //this.prevScreen = prevScreen;
+
+        // Ashley ECS
+        engine = new Engine();
+        renderSystem = new RenderSystem(batch);
+        engine.addSystem(renderSystem);
+        try {
+            engine.addEntity(player.getEntity());
+            System.out.println("Success in adding player entity to engine.");
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+        }
+    }
 
     @Override
     public void show() {
+        //Stage should control input:
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
-
+        //Clear the screen
+        Gdx.gl.glClearColor(0, 0, .16f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        engine.update(delta);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
     }
 
     @Override
@@ -36,6 +106,9 @@ public class GameView implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        skin.dispose();
+        atlas.dispose();
+        batch.dispose();
     }
 }
