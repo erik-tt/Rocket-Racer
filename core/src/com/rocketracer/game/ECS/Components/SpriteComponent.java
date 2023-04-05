987644bf822d1ea715635f4ec4ff5f66824b2dd2
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-04-05 10:43:33 +0200

    [#12] chore: update sprite component

--- core/src/com/rocketracer/game/ECS/Components/SpriteComponent.java
+++ core/src/com/rocketracer/game/ECS/Components/SpriteComponent.java
@@ -1,13 +1,19 @@
 package com.rocketracer.game.ECS.Components;
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class SpriteComponent implements Component {
     // --- Attributes ---
     public Sprite sprite;
+    public Texture texture;
+
+    // TODO: Support animations etc?
 
     // --- Constructor ---
-    public SpriteComponent(Sprite sprite) {
-        this.sprite = sprite;
+    public SpriteComponent(Texture texture, float setScale) {
+        this.texture = texture;
+        this.sprite = new Sprite(texture);
+        this.sprite.setSize(texture.getWidth(), texture.getHeight());
     }
 }
