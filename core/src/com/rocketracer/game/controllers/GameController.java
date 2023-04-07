6e36200eee36b578aa011038d8dcc52e441578cc
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2023-04-07 14:09:35 +0200

    #15 added controller methods for touch handeling

--- core/src/com/rocketracer/game/controllers/GameController.java
+++ core/src/com/rocketracer/game/controllers/GameController.java
@@ -1,8 +1,57 @@
 package com.rocketracer.game.controllers;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 
-public class GameController {
+public class GameController implements InputProcessor{
 
+    private boolean isDragged;
+
+    // The first three methods are for keyboards and is not relevant
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    //---------------------------------------
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (button != Input.Buttons.LEFT || pointer > 0) return false;
+        isDragged = true;
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        if (button != Input.Buttons.LEFT || pointer > 0) return false;
+        isDragged = false;
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        if (!isDragged) return false;
+        return true;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
 }
