6aa63c75ba6869d14165ea996122627efd20dee5
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2023-04-07 16:50:14 +0200

    #15 create barebones movement sys and typeC

---
+++ core/src/com/rocketracer/game/ECS/Systems/MovementSystem.java
@@ -0,0 +1,36 @@
+package com.rocketracer.game.ECS.Systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.rocketracer.game.ECS.Components.PositionComponent;
+import com.rocketracer.game.ECS.Components.TypeComponent;
+import com.rocketracer.game.ECS.Components.VelocityComponent;
+
+public class MovementSystem extends IteratingSystem {
+
+    private ComponentMapper<VelocityComponent> vMapper;
+    private ComponentMapper<TypeComponent> tMapper;
+
+    public MovementSystem() {
+        super(Family.all(VelocityComponent.class, TypeComponent.class).get());
+        vMapper = ComponentMapper.getFor(VelocityComponent.class);
+        tMapper = ComponentMapper.getFor(TypeComponent.class);
+
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        TypeComponent object = tMapper.get(entity);
+        VelocityComponent velocity = vMapper.get(entity);
+
+        //TODO
+        //Create a more complex movement system based on type and gameplay difficulty
+        if (object == TypeComponent.OBSTICAL) {
+            int speedY = -3;
+            velocity.y += speedY;
+        }
+
+    }
+}
