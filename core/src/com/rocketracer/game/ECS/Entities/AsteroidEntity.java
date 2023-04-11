2e6bf34bcb189d7d06d0d64ed54a583e5f80e5b2
emilisfr <emilisfr@stud.ntnu.no>
2023-04-11 13:07:23 +0200

    [#3] created obstacles entities

---
+++ core/src/com/rocketracer/game/ECS/Entities/AsteroidEntity.java
@@ -0,0 +1,33 @@
+package com.rocketracer.game.ECS.Entities;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.rocketracer.game.ECS.Components.PositionComponent;
+import com.rocketracer.game.ECS.Components.SpriteComponent;
+import com.rocketracer.game.ECS.Components.VelocityComponent;
+
+
+
+public class AsteroidEntity {
+
+    private Entity entity;
+    private SpriteComponent spriteComponent;
+    private PositionComponent positionComponent;
+    private VelocityComponent velocityComponent;
+    private Texture asteroid = new Texture(Gdx.files.internal("asteroid.png"));
+
+    public AsteroidEntity(){
+        this.entity = new Entity();
+        this.spriteComponent = new SpriteComponent(asteroid,1);
+        this.positionComponent = new PositionComponent(0,0);
+        this.velocityComponent = new VelocityComponent(0,0);
+
+        entity.add(spriteComponent);
+        entity.add(positionComponent);
+        entity.add(velocityComponent);
+    }
+
+    public Entity getEntity(){ return entity;}
+
+}
