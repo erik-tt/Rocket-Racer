e5469a8477206ce0d9e6261ce275cf51c4abffb1
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2023-04-11 11:30:27 +0200

    #24 created collision sys

--- core/src/com/rocketracer/game/ECS/Components/TypeComponent.java
+++ core/src/com/rocketracer/game/ECS/Components/TypeComponent.java
@@ -3,7 +3,7 @@ package com.rocketracer.game.ECS.Components;
 import com.badlogic.ashley.core.Component;
 
 public enum TypeComponent implements Component {
-    OBSTICAL,
+    OBSTACLE,
     POWERUP,
     ROCKET
 }
