0695fbbcb4df2550553f4725cf939c9ef2b3cce3
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2023-04-17 10:46:58 +0200

    22 configured project for junit

---
+++ core/src/test/java/com/rocketracer/game/ECS/Entities/BirdEntityTest.java
@@ -0,0 +1,11 @@
+package com.rocketracer.game.ECS.Entities;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BirdEntityTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(42, 19+23);
+    }
+}
