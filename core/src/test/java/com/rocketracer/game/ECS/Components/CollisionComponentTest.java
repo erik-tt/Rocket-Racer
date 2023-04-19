931b2aedb56a759f51f383575774b5005ca69496
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2023-04-19 09:57:31 +0200

    #22 collision component test

--- core/src/test/java/com/rocketracer/game/ECS/Components/CollisionComponentTest.java
+++ core/src/test/java/com/rocketracer/game/ECS/Components/CollisionComponentTest.java
@@ -2,12 +2,14 @@ package com.rocketracer.game.ECS.Components;
 
 import static junit.framework.TestCase.assertNotNull;
 
+import com.rocketracer.game.ECS.Entities.TestEntity;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class CollisionComponent {
 
-    private CollisionComponent collisionComponent;
+    private CollisionComponent collisionComponent = new CollisionComponent();
 
     @Before
     public void setup() {
@@ -17,5 +19,11 @@ public class CollisionComponent {
     @Test
     public void testInit() {
         assertNotNull(new CollisionComponent());
+    }
+
+    @Test
+    public void testCollisionEntity() {
+        TestEntity testEntity = new TestEntity();
+
     }
 }
